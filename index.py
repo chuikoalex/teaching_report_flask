af543040cdff9b12a9f303dec5523f718837b816
chuikoalex <Chuiko-Alex@yandex.ru>
2023-03-14 02:21:33 +0300

    Starting the development of the authorization and database connection. v0.0.1

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "my mega secret key"
 
+user = "Alex"
+password = "123456"
+
 
 @app.route("/")
 def index():
-    data = {"username": "Alex", "title": "Главная страница"}
+    data = {"username": user, "title": "Главная страница"}
     return render_template("index.html", **data)
 
 
