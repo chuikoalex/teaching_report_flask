404e14ad53854d75958226e02bb19e04dbea0874
chuikoalex <Chuiko-Alex@yandex.ru>
2023-03-20 05:06:06 +0300

    User registration and authorization done v0.0.5

--- data/users.py
+++ data/users.py
@@ -2,17 +2,19 @@ import datetime
 
 import sqlalchemy
 from sqlalchemy import orm
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    login = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=False)
+    login = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=False)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
     admin = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
@@ -22,3 +24,9 @@ class User(SqlAlchemyBase):
 
     def __repr__(self):
         return f"{self.id}: {self.name} ({self.email}) - '{self.login}'."
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- index.py
+++ index.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect, flash
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, url_for, render_template, request, redirect, flash, session
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 
 from loginform import LoginForm
+from registerform import RegisterForm
 from data import db_session
 from data.users import User
 from data.events import Event
@@ -9,17 +10,17 @@ from data.courses import Course
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "my mega secret key"
-
-USERS = {"Alex": "123456"}
-user2 = ""
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route("/")
 def index():
-    if user2 == "":
-        return redirect('/sign_in')
+    if not current_user.is_authenticated:
+        return redirect(url_for('sign_in'))
     db_sess = db_session.create_session()
     data = {"title": "Главная страница",
+            "username": session['username'],
             "events": db_sess.query(Event)
             }
     return render_template("index.html", **data)
@@ -28,53 +29,91 @@ def index():
 @app.route('/sign_in', methods=['GET', 'POST'])
 def sign_in():
     form = LoginForm()
-    if form.validate_on_submit():
-        if form.username.data in USERS:
-            if form.password.data == USERS[form.username.data]:
-                global user2
-                user2 = form.username.data
-                return redirect('/user_page')
+    if request.method == "POST":
+        if form.validate_on_submit():
+            db_sess = db_session.create_session()
+            user = db_sess.query(User).filter(User.login == form.login.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user, remember=form.remember_me.data)
+                return redirect("/")
             else:
-                flash("Пароль неверный.")
-        else:
-            flash("Такого пользователя не существует.")
+                flash("Логин и/или пароль не верны!")
+                return redirect(url_for('sign_in'))
     return render_template('sign_in.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            db_sess = db_session.create_session()
+            if db_sess.query(User).filter(User.login == form.login.data).first():
+                flash("Такой пользователь уже существует!")
+                return redirect('/register')
+            if db_sess.query(User).filter(User.email == form.email.data).first():
+                flash("Такая почта уже есть в базе данных!")
+                return redirect('/register')
+            if form.password.data != form.password_again.data:
+                flash("Пароли не совпадают!")
+                return redirect('/register')
+            user = User()
+            user.login = form.login.data
+            user.name = form.name.data
+            user.email = form.email.data
+            user.set_password(form.password.data)
+            db_sess.add(user)
+            db_sess.commit()
+            return redirect(url_for('sign_in'))
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 @app.route("/user_page")
 def user_page():
-    data = {"username": user2, "title": "Страница пользователя"}
+    data = {"username": session['username'], "title": "Страница пользователя"}
     return render_template("user_page.html", **data)
 
 
 @app.route("/exit_lk")
 def exit_lk():
-    global user2
-    user2 = ""
-    return redirect('/')
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 if __name__ == "__main__":
     db_session.global_init("db/report_tch.db")
+    app.run(debug=True)
+
+    # db_session.global_init("db/report_tch.db")
+
+    # app.run(debug=True)
+    # login_manager = LoginManager()
+    # login_manager.init_app(app)
 
     # db_sess = db_session.create_session()
 
     # user = User()
     # user.name = "Попов Александр Степанович"
     # user.login = "popov"
-    # user.email = "admin@tch_mail.ru"
-    # user.hashed_password = "123456"
+    # user.email = "admin@email.ru"
     # user.admin = True
+    # user.set_password("123456")
     # user2 = User()
     # user2.name = "Глушков Виктор Михайлович"
     # user2.login = "glush"
-    # user2.email = "glush@tch_mail.ru"
-    # user2.hashed_password = "123456"
+    # user2.email = "glush@email.ru"
+    # user2.set_password("123456")
     # user3 = User()
     # user3.name = "Котельников Владимир Александрович"
     # user3.login = "kotel"
-    # user3.email = "kotel@tch_mail.ru"
-    # user3.hashed_password = "123456"
+    # user3.email = "kotel@email.ru"
+    # user3.set_password("kotel@email.ru")
     # db_sess.add(user)
     # db_sess.add(user2)
     # db_sess.add(user3)
@@ -112,14 +151,12 @@ if __name__ == "__main__":
     # db_sess.add(event)
     # db_sess.add(event2)
     # db_sess.add(event3)
-
+    #
     # db_sess.commit()
 
-    # ----- view db --------
     # for user in db_sess.query(User).all():
     #     print(user)
+    #     print("Password:", user.check_password("123456"))
     # print()
     # for course in db_sess.query(Course).all():
     #     print(course)
-
-    app.run(debug=True)
